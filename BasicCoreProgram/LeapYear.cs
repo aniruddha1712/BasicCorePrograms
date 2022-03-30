b019454737e1de4f6ea80d125a8890d60717c222
Aniruddha <aniruddha1712@gmail.com>
2022-03-30 20:32:04 +0530

    [Aniruddha]Added: program to find whether given year is leapyear or not

---
+++ BasicCoreProgram/LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    class LeapYear
+    {
+        public static void IsLeapYear()
+        {
+            Console.WriteLine("Enter any year:");
+            int year=Convert.ToInt32(Console.ReadLine());
+
+            if(year%400==0)
+            {
+                Console.WriteLine(year+" is a leap year");
+            }
+            else if(year%4==0 && year%100!=0)
+            {
+                Console.WriteLine(year+" is a leap year");
+            }
+            else
+                Console.WriteLine(year+" is not a leap year");
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -11,12 +11,18 @@ namespace BasicCoreProgram
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Basic core programs of C#");
-            Console.WriteLine("1:Flipcoin\n");
+            Console.WriteLine("1:Flipcoin\n2:LeapYear\n3:");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     FlipCoin.FindFlipCount();
+                    break;
+                case 2:
+                    LeapYear.IsLeapYear();
+                    break;
+                default:
+                    Console.WriteLine("Enter valid input");
                     break;
             }
             Console.ReadLine();
