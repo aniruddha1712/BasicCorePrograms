725a636f526db9a044327e9c1a29cfff2f52cfe8
Aniruddha <aniruddha1712@gmail.com>
2022-03-31 13:13:21 +0530

    [Aniruddha]Added: Program to find largest among three numbers

---
+++ BasicCoreProgram/LargestAmong3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    class LargestAmong3
+    {
+        public static void FindLargest()
+        {
+            int temp;
+            Console.WriteLine("Enter 1st number");
+            int num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int num2=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 3rd number");
+            int num3=int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+                temp = num1;
+            else
+                temp = num2;
+            if (temp > num3)
+                Console.WriteLine("{0} is the largest", temp);
+            else
+                Console.WriteLine("{0} is the largest", num3);
+
+                
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -11,7 +11,7 @@ namespace BasicCoreProgram
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Basic core programs of C#");
-            Console.WriteLine("1:Flipcoin\n2:LeapYear\n3:FindPOwerOf2\n4:HarmonicNumber\n5:PrimeNumbers\n6:FindQuotientAndRemainder\n7:SwapNumbers\n8:OddEven\n9:CheckVowel\n10:");
+            Console.WriteLine("1:Flipcoin\n2:LeapYear\n3:FindPOwerOf2\n4:HarmonicNumber\n5:PrimeNumbers\n6:FindQuotientAndRemainder\n7:SwapNumbers\n8:OddEven\n9:CheckVowel\n10:LaregestAmongThree");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -41,6 +41,9 @@ namespace BasicCoreProgram
                     break;
                 case 9:
                     CheckVowel.CheckAlphabet();
+                    break;
+                case 10:
+                    LargestAmong3.FindLargest();
                     break;
                 default:
                     Console.WriteLine("Enter valid input");
